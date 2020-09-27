03726253d971fe323a73710dbacbeeb384d7092a
tinabeattysmith <tinatest2020@gmail.com>
2020-09-26 23:51:42 -0400

    9/26 EOD

--- index.js
+++ index.js
@@ -1,38 +1,89 @@
 //BaseURL for ergast API
-let baseURL = "http://ergast.com/api/f1/drivers.json";
+let baseURL = "http://ergast.com/api/f1";
 //console.log(baseURL); // checking to make sure script file connected to HTML
+let seasonNumber;
+let url;
 
-const qSeries = document.getElementById('inputF1Series');
+//declaring DOM elements to be manipulated
 const qSeason = document.getElementById('inputF1Season');
-const qDriver = document.getElementById('inputF1Driver');
 const queryForm = document.querySelector('form');
-
 
 //add eventlistner to the form
 queryForm.addEventListener('submit', fetchResults);
 
 function fetchResults(e) {
     e.preventDefault();
-    console.log('you clicked me');
+    //console.log('you clicked me');
 
-    //set default value of series to f1 because it is required in url
-    let qSeries = 'f1';
-    console.log('Series: ', qSeries);
+        // storing input value of Season field as a variable
+        if(qSeason.value != '') {
+            seasonNumber=qSeason.value;
+        } else{
+            seasonNumber = '';
+        }
+        //console.log("season", seasonNumber);
+       
+        //updating the base url with season variable
+        if(seasonNumber === '' ){
+            url=`${baseURL}/drivers.json`
+        } else {
+            url=`${baseURL}/${seasonNumber}/drivers.json`
+        }
+        //console.log("URL", url);
 
-     fetch(baseURL)
+     fetch(url)
         .then (function(result) {
             //console.log("Result:", result);
             return result.json();
         })
         .then (function(json) {
-            console.log("JSON: ", json);
+            //console.log("JSON: ", json);
             displayResults(json);
         });
+    };
+        
+    function displayResults(json) {
+        let driver = json.MRData.DriverTable.Drivers;
+        console.log("Drivers:", driver);
+        let table = document.querySelector("table");
 
-        function displayResults(json) {
-            let driver = json.MRData.DriverTable;
-            console.log("Drivers:", driver);
-            };
+  
+        if(driver.length === 0) {
+            console.log("No Results");
+
+            } else {    
+                for(let i = 0; i < driver.length; i++) { 
+                //console.log(i); // checking to see loop is working
+                let current = driver[i]; // each time the index is incrememted, it is set as current.
+                console.log("Current:", current);
+                let data = Object.keys(driver[0]);
+                            
+                function generateTableHead(table, data) {
+                    let thead = table.createTHead();
+                    let row = thead.insertRow();
+                        for(let key of data){     
+
+                            let th = document.createElement("th"); 
+                            let text = document.createTextNode(key);
+                            th.appendChild(text);
+                            row.appendChild(th);
+                        };
+                    };   
+                    
+                function generateTable(table, data) {                    
+                        for(let element of data){
+                            let row = table.insertRow();
+                            for (key in element) {
+                                let cell = row.insertCell();
+                                let text = document.createTextNode(element[key]);
+                                cell.appendChild(text);
+                        };
+                    }; 
+                };
+                                    
+                    generateTableHead(table, data);
+                    generateTable(table, driver);
+                    
+                };
+            };            
         };
-
-   
